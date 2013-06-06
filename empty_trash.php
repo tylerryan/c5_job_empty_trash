c625673f73c7aedb1a6d6757e2dbfd6dbd689338
Mike Lay <michael.lay.email@gmail.com>
2013-06-06 10:34:20 -0700

    initial commit

---
+++ empty_trash.php
@@ -0,0 +1,34 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+
+class EmptyTrash extends Job {
+
+	public function getJobName() {
+		return t('Empty Trash');
+	}
+
+	public function getJobDescription() {
+		return t('Empty trash 10 pages at a time');
+	}
+
+	public function run() {
+
+		$count = 0;
+
+		$pl = new PageList;
+		$pl->filterByParentID(Page::getByPath('/!trash')->getCollectionID());
+		$pages = $pl->get(10);
+		$total = $pl->getTotal();
+
+		while ($page = array_pop($pages)) {
+			$page->delete();
+			$count++;
+		}
+
+		$remaining = $total - $count;
+		if ($remaining < 0) $remaining = 0;
+
+		return t('%s pages emptied from trash. %s remain.', $count, $remaining);
+	}
+
+}
