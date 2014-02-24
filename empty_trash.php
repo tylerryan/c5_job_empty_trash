cff4c6c955a56a446376979cadf90ca60e90275e
tylerryan <ryan@concrete5.org>
2014-02-24 14:11:28 -0800

    made job queueable

--- empty_trash.php
+++ empty_trash.php
@@ -1,8 +1,11 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
 
-class EmptyTrash extends Job {
+class EmptyTrash extends QueueableJob {
 
+	public $jSupportsQueue = true;
+
+	
 	public function getJobName() {
 		return t('Empty Trash');
 	}
@@ -11,6 +14,8 @@ class EmptyTrash extends Job {
 		return t('Empty trash 10 pages at a time');
 	}
 
+	//public function run() {}
+	/*
 	public function run() {
 
 		$count = 0;
@@ -35,5 +40,49 @@ class EmptyTrash extends Job {
 
 		return t('%s pages emptied from trash. %s remain.', $count, $remaining);
 	}
+	*/
+	
+	public function start(Zend_Queue $q) {
+		$pl = new PageList;
+		$pl->filterByParentID(Page::getByPath('/!trash')->getCollectionID());
+		$pl->ignorePermissions(true);
+		$pl->displayUnapprovedPages();
+		$pl->includeSystemPages();
+		$pl->includeInactivePages();
+		$pages = $pl->get();
+		$total = $pl->getTotal();
 
+		foreach($pages as $page) {
+			$deletePages= array();
+			$includeThisPage = true;
+			if ($page->getCollectionPath() == TRASH_PAGE_PATH) {
+				$includeThisPage = false;
+			}
+			$deletePages = $page->populateRecursivePages($deletePages, array('cID' => $page->getCollectionID()), $page->getCollectionParentID(), 0, $includeThisPage);
+			
+			// now, since this is deletion, we want to order the pages by level, which
+			// should get us no funny business if the queue dies.
+			usort($deletePages, array('Page', 'queueForDeletionSort'));
+			foreach($deletePages as $d) {
+				$q->send(Loader::helper('json')->encode(array('cID'=>$d['cID'])));
+			}
+		}
+	}
+	
+	public function processQueueItem(Zend_Queue_Message $msg) {
+		$args = Loader::helper('json')->decode($msg->body);
+		
+		$cID = $args->cID;
+		if(is_numeric($cID) && $cID) {
+			$page = Page::getByID($cID);
+			if($page instanceof Page && $page->getCollectionID()) {
+				$page->delete();
+			}
+		}
+	}
+	
+	public function finish(Zend_Queue $q) {
+		return t('The trash has been emptied.');
+	}
+	
 }
